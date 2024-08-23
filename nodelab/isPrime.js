114a81bdc6eebaf083f6984cd28a881d486d6e1b
Abhinav Pandey <abhinavpandey56393@gmail.com>
2024-08-23 13:15:01 +0530

    add program of checking prime number

---
+++ nodelab/isPrime.js
@@ -0,0 +1,16 @@
+function checkprime(n){
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            count++;
+         }
+         
+    }
+    if(count == 2){
+        console.log(n + " is a prime number");
+     }
+     else{
+        console.log(n + " is not a prime number");
+     }
+}   
+checkprime(9);
